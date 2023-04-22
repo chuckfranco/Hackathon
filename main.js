b2c1742f4722001b82af916de3a25cdf4156da95
Chuck Franco <charlesfrancofranco@Chucks-MacBook-Pro-2.local>
2023-04-22 11:20:23 -0500

    typerwriter animation

--- main.js
+++ main.js
@@ -7,19 +7,30 @@ const options = {
 };
 
 
-const lookup = (searchTerm) => { fetch(`https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=${searchTerm}`, options)
-.then(response => response.json())
-.then(data => {
-    console.log(data)
-    const body = document.querySelector('body')
-    const definition = data.list[0].definition.replace(/[^a-zA-Z0-9.\n'"!?$&() ]/g,'')
-
-    const newParagraph = document.createElement('p')
-    newParagraph.innerHTML = definition
-    newParagraph.style.color = '#a1512b'
-    body.insertAdjacentElement("beforeend", newParagraph)
-})
-.catch(err => console.error(err))
+const lookup = (searchTerm) => { 
+    let index = 0;
+    let speed = 50;
+    fetch(`https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=${searchTerm}`, options)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            const body = document.querySelector('body')
+            const definition = data.list[0].definition.replace(/[^a-zA-Z0-9.\n'"!?$&() ]/g,'')
+            const newParagraph = document.createElement('p')
+            function typeWriter() {
+                if (index < definition.length) {
+                    newParagraph.innerHTML += definition.charAt(index);
+                    index++;
+                    setTimeout(typeWriter, speed);    
+                }
+                // index = 0;
+            }
+            // newParagraph.innerHTML = definition
+            typeWriter();
+            newParagraph.style.color = '#a1512b'
+            body.insertAdjacentElement("beforeend", newParagraph)
+        })
+        .catch(err => console.error(err))
 };
 
 const searchButton = document.querySelector('#btn')
